42d82181adb0d5adf954ff9377d455799d384225
Ryan Haughton <rahaughton@g.hmc.edu>
2022-04-11 11:42:09 -0700

    Initial commit to this repo- event app

--- src/index.js
+++ src/index.js
@@ -1,14 +1,55 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { createRoot } from 'react-dom/client';
+import App from './Components/App';
 import reportWebVitals from './reportWebVitals';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import { ThemeProvider, createTheme } from '@mui/material';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const httpLink = createHttpLink({
+  uri: 'https://event-app.hasura.app/v1/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      'x-hasura-admin-secret':
+        'ccVceaUQ7IsFx73nsbDzLzgrbX1aTx3c5dZT9sdZI5ddLCEuXnIcWMkecyFX32zY',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+const theme = createTheme({
+  components: {
+    MuiDivider: {
+      styleOverrides: {
+        root: {
+          backgroundColor: '#e6e6e4',
+        },
+      },
+    },
+  },
+});
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(
+  <ApolloProvider client={client}>
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </ApolloProvider>,
 );
 
 // If you want to start measuring performance in your app, pass a function
